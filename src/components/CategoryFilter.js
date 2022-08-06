714e35cf4c4e6c5852bb7788b222220d8da30b62
PRINCE KUMABIO <kumabio.prince@student.moringaschool.com>
2022-08-06 13:16:20 +0000

    assignment completed

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,27 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ cats, onBtnClicked }) {
+  function handleCatBtn(event) {
+    const btnElement = event.target.parentElement.children;
+    for (let i = 1; i < btnElement.length; i++) {
+      btnElement[i].className = "";
+    }
+
+    let catName = event.target.textContent;
+
+    event.target.className = "selected";
+
+    onBtnClicked(catName);
+
+  }
+
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {
+        cats.map((cat, index) => <button key={index} onClick={handleCatBtn}>{cat}</button>)
+      }
     </div>
   );
 }
